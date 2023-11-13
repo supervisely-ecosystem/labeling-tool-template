a0eb56593f504cbe6552e665eb7e516ed99f5d09
iwatkot <iwatkot@gmail.com>
2023-11-13 15:28:44 +0300

    Debug posts.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import supervisely as sly
+from fastapi import Request
 
 from supervisely.app.widgets import Container, Text
 
@@ -7,3 +8,26 @@ test_text = Text("Hello, World!")
 layout = Container(widgets=[test_text])
 
 app = sly.Application(layout=layout)
+
+server = app.get_server()
+
+
+@server.post("/")
+def debug(request: Request):
+    print(f"Request: {request}")
+
+    return {"status": "ok"}
+
+
+# @server.post("/brush_tool_released")
+# def brush_tool_released(request: Request):
+#     print(f"Brush tool released, request: {request}")
+
+#     return {"status": "ok"}
+
+
+# @server.post("/eraser_tool_released")
+# def eraser_tool_released(request: Request):
+#     print(f"Eraser tool released, request: {request}")
+
+#     return {"status": "ok"}
