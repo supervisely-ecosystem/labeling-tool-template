dbad5f2a4e8b00d36549e91c30c26be74740d09d
iwatkot <iwatkot@gmail.com>
2023-11-24 11:52:35 +0300

    Changed to the new event.

--- src/main.py
+++ src/main.py
@@ -37,9 +37,8 @@ if sly.is_development():
     sly_app_development.supervisely_vpn_network(action="up")
     sly_app_development.create_debug_task(team_id, port="8000")
 
-# Creating cache for project meta and images as numpy arrays.
+# Creating cache for project meta to avoid unnecessary requests to Supervisely API.
 project_metas = {}
-images_cache = {}
 
 
 @app.event(sly.Event.Brush.DrawLeftMouseReleased)
@@ -58,47 +57,35 @@ def brush_left_mouse_released(api: sly.Api, event: sly.Event.Brush.DrawLeftMouse
     # Get project meta (using simple cache) to create sly.Label object.
     project_meta = get_project_meta(api, event.project_id)
 
-    # Get image numpy array (using simple cache) to process the label.
-    image_np = get_image_np(api, event.image_id)
+    # Retrieving object class from project meta to create sly.Label object later.
+    obj_class = project_meta.get_obj_class_by_id(event.class_id)
 
-    # Get sly.Label object with actual mask from Supervisely API.
-    label = api.annotation.get_label_by_id(event.label_id, project_meta)
+    # Processing the mask. You need to implement your own logic in the process function.
+    new_mask = process(event.mask)
 
-    # Processing the label.
-    # You need to implement your own logic in the process_label function.
-    new_label = process(label, image_np)
+    # Creating a new label with the updated mask.
+    label = sly.Label(geometry=sly.Bitmap(data=new_mask.astype(bool)), obj_class=obj_class)
 
     # Upload the label with the updated mask to Supervisely platform.
-    api.annotation.update_label(event.label_id, new_label)
+    api.annotation.update_label(event.label_id, label)
 
     processing_text.hide()
 
 
-def process(label: sly.Label, image_np: np.ndarray) -> sly.Label:
-    """Processing sly.Label object and returning the processed one.
+def process(mask: np.ndarray) -> np.ndarray:
+    """Processing the mask.
 
-    :param label: sly.Label object to process.
-    :type label: sly.Label
-    :param image_np: Image numpy array.
-    :type image_np: np.ndarray
-    :return: Processed sly.Label object.
-    :rtype: sly.Label
+    :param mask: Mask to process.
+    :type mask: np.ndarray
+    :return: Processed mask.
+    :rtype: np.ndarray
     """
-    # In this tutorial we'll be working with masks, but you can implement your own
-    # logic to edit the label (e.g. work with geometry, tags).
-
-    # Retrieving image size from numpy array to create a full image size mask.
-    image_size = image_np.shape[:2]
-
-    # Object mask is stored in optimized form, let's unpack it.
-    mask = label.get_mask(image_size)
-
     # Reading the strength of the dilation operation from the UI
     # and applying it to the mask.
     dilation = cv2.dilate(mask.astype(np.uint8), None, iterations=dilation_strength.get_value())
 
-    # Returning a new label with the processed data.
-    return label.clone(geometry=sly.Bitmap(data=dilation.astype(bool)))
+    # Returning a new mask.
+    return dilation
 
 
 def get_project_meta(api: sly.Api, project_id: int) -> sly.ProjectMeta:
@@ -117,27 +104,6 @@ def get_project_meta(api: sly.Api, project_id: int) -> sly.ProjectMeta:
     else:
         project_meta = project_metas[project_id]
     return project_meta
-
-
-def get_image_np(api: sly.Api, image_id: int) -> np.ndarray:
-    """Retrieving image numpy array: if cached, then return from cache, else retrieve from Supervisely API.
-
-    :param api: Supervisely API object.
-    :type api: sly.Api
-    :param image_id: Image ID for which to retrieve numpy array.
-    :type image_id: int
-    :return: Image numpy array.
-    :rtype: np.ndarray
-    """
-    if len(images_cache) > 100:
-        # Clearing the cache if it's too big.
-        images_cache.clear()
-    if image_id not in images_cache:
-        image_np = api.image.download_np(image_id)
-        images_cache[image_id] = image_np
-    else:
-        image_np = images_cache[image_id]
-    return image_np
 
 
 @need_processing.value_changed
